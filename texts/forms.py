9dd73e3b35f36f038209a9a0a67be355f846e304
spacesquid2000 <devon.dear@gmail.com>
2016-03-28 22:47:57 -0500

    LMDapp, second commit

---
+++ texts/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class DeedForm(forms.Form):
+    deed = forms.CharField(label = 'Meritorious Deed', max_length = 300)
+    deed = forms.CharField(label = 'Meritorious Deed', max_length = 300)
+    deed = forms.CharField(label = 'Meritorious Deed', max_length = 300)
+    deed = forms.Charfield(label = 'Demerit', max_length = 300)
--- texts/views.py
+++ texts/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import loader
+from .forms import DeedForm
 import random
 from random import choice
 
@@ -52,7 +54,7 @@ def diarydate(request):
             'donated clothes worth 100 coins': '1',
             'donated coffins worth 100 coins': '1'}
 
-    s2p1 = ('As I was strolling through my fields, I ', 'As I was walking in the town square, I ', 'During my visit to the teahouse, I ', 'While meandering through the countryside, I')
+    s2p1 = ('As I was strolling through my fields, I ', 'After meeting an old friend, I', 'After having argued with my concubine, I','As I was walking in the town square, I ', 'During my visit to the teahouse, I ', 'While meandering through the countryside, I')
     s2_var1 = random.choice(s2p1) #sets the initial setting
     mer_var1 = random.choice(list(ledger_mer.keys())) #selects the first meritorious act
     done_deeds = (mer_var1) #saves the done meritorious acts to a list
@@ -67,5 +69,12 @@ def diarydate(request):
     if s1_emotions_var1 == 'a nightmare':
         print ('And alas, I did so many bad deeds!')
 
-    html = "The date is %s %s, %s. Today was %s and I feel %s. %s %s. In addition I %s." % (month1, day1, year1, s1_emotions_var1, s1_emotions[s1_emotions_var1], s2_var1, mer_var1, random.choice(list(ledger_mer.keys())))
+    html = "This is the week of %s %s, %s. My days were %s and I feel %s. %s %s. In addition I %s." % (month1, day1, year1, s1_emotions_var1, s1_emotions[s1_emotions_var1], s2_var1, mer_var1, random.choice(list(ledger_mer.keys())))
     return render_to_response('texts/diarydate.html', {'html': html})
+
+def FormCalculate(): #this function should add them all up, doesn't work
+    response = input('Enter the deed: ')
+    d1 = ledger_mer[response]
+    if d1 in ledger_mer.keys():
+        return int(d1)
+    print("This action's merit is  " + x)
